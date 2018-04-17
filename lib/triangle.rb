a59a98968fbd0fbdd291c542d4d9900aa170e418
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-17 17:03:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   attr_accessor :length_1, :length_2, :length_3
-
+  
   def initialize(length_1, length_2, length_3)
     @length_1 = length_1
     @length_2 = length_2
@@ -8,6 +8,13 @@ class Triangle
   end
 
   def kind
-
+    if length_1 == length_2 && length_1 == length_3
+      return :equilateral
+    elsif length_1 == length_2 && length_1 != length_3 || length_1 == length_3 && length_1 != length_2
+      return :isosceles
+    elsif length_1 != length_2 && length_1 != length_3
+      return :scalene
+    end
+      
   end
 end
