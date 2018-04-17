50681bcb83ba1c51a7ab19433555723587be059b
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-17 16:58:52 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,13 @@
 class Triangle
+  attr_accessor :length_1, :length_2, :length_3
+
   def initialize(length_1, length_2, length_3)
     @length_1 = length_1
     @length_2 = length_2
     @length_3 = length_3
+  end
+
+  def kind
+
   end
 end
